56be09feb66c99aed19514051c3e7d46cac75919
17030752 <davidenriqueprogramacion@gmail.com>
2021-05-08 15:19:50 -0500

    cambios en las plantillas blade de blog contacto y servicio

--- example-app/resources/views/services.blade.php
+++ example-app/resources/views/services.blade.php
@@ -1,13 +1,28 @@
-@extends('layouts.template1')
-
-@section('title', 'Home')
-
+@extends('layouts.template2')
+@section('tittle' ,'Services')
 @section('sidebar')
     @parent
-
-    <p>This is appended to the master sidebar.</p>
 @endsection
-
 @section('content')
-    <p><h1>This is my Services page.</h1></p>
+    @parent
+<div class="row">
+<div class="box">
+<div class="col-lg-12">
+<hr>
+<h2 class="intro-text text-center"> services <strong>bussiness casual</strong></h2>
+</div>
+<div class="col-md-12">
+hrere goes services
+<ul>
+@foreach ($services as $service )
+    <li>{{$service->name}}({{$service->description}})</li>
+@endforeach
+</ul>
+</div>
+<div class="clearfix"></div>
+</div>
+</div>
+@endsection
+@section('footer')
+    @parent
 @endsection
